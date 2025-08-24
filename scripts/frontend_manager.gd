1b1af5fd44c53cbea11f54bea8e2eae5bb372cda
tofimega <fintaistvan05@gmail.com>
2025-08-24 13:34:38 +0200

    hud works kinda

--- scripts/frontend_manager.gd
+++ scripts/frontend_manager.gd
@@ -10,10 +10,10 @@ var interfaces: Dictionary[int, UserInput]={} # player id, player Interface
 const UI_PACKED: PackedScene = preload("res://scenes/user_input/user_input.tscn") 
 
 func add_user_player_interface(id: int) -> UserInput: 
-	var player_input: UserInput = UI_PACKED.instantiate()
+	var player_input: UserInput = get_node("/root/GameScene/HUD/UserInput") #TODO: there has to be a better way
 	player_input.player_id=id
 	interfaces[id] = player_input
-	get_hud().add_child(player_input)
+	#get_hud().add_child(player_input)
 	return player_input
 
 
--- scripts/player_controller/user_player_controller.gd
+++ scripts/player_controller/user_player_controller.gd
@@ -5,13 +5,13 @@ extends PlayerController
 
 func _init(player: Player) -> void:
 	super(player)
-	#FrontendManager.add_user_player_interface(player.id).user_bet.connect(_send_bet)
+	FrontendManager.add_user_player_interface(player.id).user_bet.connect(_send_bet)
 	
 	
 	
 func _send_bet(bet: Bet) -> Bet:
 	
-	#FrontendManager.interfaces[player.id].enabled=false
+	FrontendManager.interfaces[player.id].enabled=false
 	# send bet
 	Logger.log_text("Player "+str(player.id)+" made bet: "+bet.Type.find_key(bet.type)+" "+str(bet.amount))
 	PokerEngine.player_bet.emit(player.id, bet)
@@ -19,7 +19,7 @@ func _send_bet(bet: Bet) -> Bet:
 	
 func my_turn() -> void:
 	Logger.log_text("Player "+str(player.id)+": waiting for user input...")
-	#FrontendManager.interfaces[player.id].enabled=true
+	FrontendManager.interfaces[player.id].enabled=true
 
 func is_human()->bool:
 	return true
